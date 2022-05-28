05974dbe8ae4412e9af3806d123a0047a4a685df
mohamedxteamspeak <bogossmr332@gmail.com>
2022-05-28 23:11:49 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,7 +15,7 @@ global.APIs = {
 
 //Api Key\\
 global.APIKeys = {
-	'https://zenzapis.xyz': '5F8F839A536C', //credit goes to sanzy yt, if api key expire, buy it and put it here?\\
+	'https://zenzapis.xyz': 'a876d4ceb6', //credit goes to sanzy yt, if api key expire, buy it and put it here?\\
 }
 
 //customize settings\\
